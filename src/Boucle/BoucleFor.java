18338cdd19a4856360fe602630adbd6f69624586
SBIA-Younes <94634321+SBIA-Younes@users.noreply.github.com>
2023-10-18 00:06:07 +0100

    add Loop

---
+++ src/Boucle/BoucleFor.java
@@ -0,0 +1,9 @@
+package Boucle;
+
+public class BoucleFor {
+    public static void main(String[] args) {
+        for (int i =1; i <= 5; i++) System.out.println("Hello World !! " + i);
+        System.out.println("--------");
+        for (int i =5; i > 0; i--) System.out.println("Hello World !! " + i);
+    }
+}
